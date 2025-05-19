7a8356845fba657c46890298547f48e547b67bf1
Joe Cox <joeyalancox@gmail.com>
2025-05-19 09:52:50 -0700

    ts testing (#11)

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,18 @@
+/// <reference types="vitest/config" />
+import react from "@vitejs/plugin-react";
 import type { UserConfig } from "vite";
 
 export default {
   root: "./frontend",
+  plugins: [react()],
+  test: {
+    environment: "happy-dom",
+    browser: {
+      provider: "playwright",
+      enabled: true,
+      headless: true,
+      instances: [{ browser: "chromium" }],
+    },
+    setupFiles: ["./test-utils/setup.ts"],
+  },
 } satisfies UserConfig;
